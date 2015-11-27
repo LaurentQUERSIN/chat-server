9ad86d82f03385db7aaba21f45e7f6d33e302c60
LaurentQUERSIN <quersin.laurent@gmail.com>
2015-11-27 15:53:38 +0100

    simple chat server plugin for stormancer

---
+++ Chat server/Chat server/ChatServer.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Concurrent;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Stormancer;
+using Stormancer.Core;
+using Stormancer.Plugins;
+using Stormancer.Server;
+
+public class ChatUserInfo
+{
+    public string User;
+}
+
+public struct ChatMessageDTO
+{
+    public ChatUserInfo UserInfo;
+    public string Message;
+}
+
+public static class ChatServerExtensions
+{
+    public static ChatServer AddChat(this ISceneHost scene)
+    {
+        return new ChatServer(scene);
+    }
+}
+
+public class ChatServerRun
+{
+    void Run(IAppBuilder builder)
+    {
+        builder.SceneTemplate("ChatRoom", scene => scene.AddChat());
+    }
+}
+
+public class ChatServer
+{
+    private ISceneHost _scene;
+    private ConcurrentDictionary<long, ChatUserInfo> UsersInfos = new ConcurrentDictionary<long, ChatUserInfo>();
+
+    void OnMessageReceived(Packet<IScenePeerClient> packet)
+    {
+        var dto = new ChatMessageDTO();
+        ChatUserInfo temp;
+
+        UsersInfos.TryGetValue(packet.Connection.Id, out temp);
+        dto.UserInfo = temp;
+        dto.Message = packet.ReadObject<string>();
+        _scene.Broadcast("chat", dto, PacketPriority.MEDIUM_PRIORITY, PacketReliability.RELIABLE);
+    }
+
+    void OnUpdateInfo(Packet<IScenePeerClient> packet)
+    {
+        var info = packet.ReadObject<ChatUserInfo>();
+
+        if (UsersInfos.ContainsKey(packet.Connection.Id) == true)
+        {
+            ChatUserInfo trash;
+            UsersInfos.TryRemove(packet.Connection.Id, out trash);
+        }
+        UsersInfos.TryAdd(packet.Connection.Id, info);
+    }
+
+    public ChatServer(ISceneHost scene)
+    {
+        _scene = scene;
+        _scene.AddRoute("UpdateInfo", OnUpdateInfo);
+        _scene.AddRoute("chat", OnMessageReceived);
+    }
+}
